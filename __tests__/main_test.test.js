371e3c7eb39024da3e14a19c09772ae0d5b51c6e
Asjal <aahmad@trinity..edu>
2023-03-04 17:37:18 -0600

    Adds framework for testing

---
+++ __tests__/main_test.test.js
@@ -0,0 +1,61 @@
+const request = require('supertest');
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const app = require('../index.js');
+
+let mongoServer;
+
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const mongoUri = mongoServer.getUri();
+    await mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+});
+
+describe('GET /', () => {
+    it('should return a hello world message', async () => {
+        const response = await request(app).get('/');
+        expect(response.status).toBe(200);
+        expect(response.body.msg).toBe('Hello World');
+    });
+});
+
+describe('GET /api', () => {
+    it('should return a hello message from captain-america', async () => {
+        const response = await request(app).get('/api');
+        expect(response.status).toBe(200);
+        expect(response.body.msg).toBe('Hello from captain-america');
+    });
+});
+
+describe('GET /api/*', () => {
+    it('should return a hello message from /api/', async () => {
+        const response = await request(app).get('/api/foo/bar');
+        expect(response.status).toBe(200);
+        expect(response.body.msg).toBe('hello this is /api/');
+    });
+});
+
+describe('POST /login', () => {
+    it('should return a valid login result', async () => {
+        const response = await request(app)
+            .post('/login')
+            .send({ user: 'student', pass: '123' });
+        expect(response.status).toBe(200);
+        expect(response.body.result).toBe(true);
+    });
+});
+
+describe('GET /testDB', () => {
+    it('should return a response without errors', async () => {
+        const response = await request(app).get('/testDB');
+        expect(response.status).toBe(200);
+    });
+});
