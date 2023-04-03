7475074dcac8a39a93c8f222791567400613a249
Nathan Early <nearly@trinity.edu>
2023-04-03 09:16:15 -0500

    schemas

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const moment = require('moment/moment');
 const bodyParser = require('body-parser');
 const { result } = require('underscore');
 const mongoose = require("mongoose");
+const User = require('./schemas/user');
+const Course = require('./schemas/course');
 
 const app = express();
 
@@ -16,59 +18,54 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// mongoose.connect(process.env.MONGODB_URI,
-//   {
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   }
-// // );
-// mongoose.connect(process.env.MONGODB_URI);
-// const connection = mongoose.connection;
 
-// connection.on('error', console.error.bind(console, 'connection error:'));
-
-app.post('/courses', (req,res) => {
-    const username = req.body.user;
-
-    // connection.once('open', async () => {
-    //     const collection = connection.db.collection("people");
-    //     collection.find({username: username}).toArray((err, data) => {
-    //         res.json({courses: data.courses});
-    //     })
-    // })
-    
-    res.json({courses: "1 2 3 4"})
-
+app.post('/api/userinfo', (req,res) => {
+    User.findOne({username: req.body.user}).exec((err, user) => {
+        res.json(user);
+        console.log(user.password);
+    });
 })
 
+app.post('/api/getcourses', (req,res) => {
+    User.findOne({username: req.body.user}).exec((err, user) => {
+        if (req.body.registration) {
+            res.json({courses: user.shoppingCart});
+        } else {
+            res.json({courses: user.courses});
+        }
+    });    
+});
 
-app.post('/login', (req, res) => {
+app.post('/api/getcourse', (req,res) => {
+    Course.findOne({courseNumber: req.body.courseName}).exec((err,course) => {
+        if (course !== null)
+            res.send(course);
+    });
+});
+
+
+app.post('/api/login', (req, res) => {
     const data = req.body;
-    const user = data.user;
-    const pass = data.pass;
-    // console.log("called");
-    
-    // const collection = connection.collection("people");
-    // const docs = collection.find({username: user}).exec();
-
-    res.json({valid: true});
-
-
+    User.findOne({username: data.user}).exec((err, user) => {
+        if (user !== null && 
+           (user.password === data.pass)) {
+                res.json({
+                    valid: true,
+                    userType: user.userType
+                });
+        } else {
+            res.json({valid: false});
+        }
+    });
 });
 
-app.get('/', (req, res) => {
-    res.json({ msg: 'Hello World' });
-});
-
-app.get('/api', (req, res) => {
-    res.json({ msg: 'Hello from captain-america' });
-});
-
-app.get('/api/*', (req, res) => {
-    res.json({ msg: 'hello this is /api/' });
-});
-
-app.listen(process.env.HOST_PORT);
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGODB_URI).then(
+    () => { 
+        app.listen(process.env.HOST_PORT); 
+        console.log(`listening on: localhost:${process.env.HOST_PORT}`);
+    },
+    (err) => { console.log("mongo db connection failed") }
+)
 
 module.exports = app;
---
+++ schemas/course.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var CourseSchema = new Schema({
+    courseNumber: String,
+    title: String,
+    professor: String,
+    time: String,
+    desc: String,
+    creditHours: Number,
+    prereqs: String,
+    enrolled: Number,
+    capacity: Number
+});
+
+var Course = mongoose.model("Course", CourseSchema);
+module.exports = Course;
