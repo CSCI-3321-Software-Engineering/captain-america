a7d64c39f82fdfab85d4c996b5f936fc5ece4cdc
Asjal <aahmad@trinity..edu>
2023-04-21 11:10:06 -0500

    Implements Logging of all actions

--- schemas/logs.js
+++ schemas/logs.js
@@ -3,9 +3,9 @@ var Schema = mongoose.Schema;
 var LogsSchema = new Schema({
     //modifications like additons / deletions of courses & students
     title: String,
-    user : String,
+    user: String,
     timeStamp: String
 });
 
-var Logs= mongoose.model("Logs", Logschema);
+var Logs = mongoose.model("Logs", LogsSchema);
 module.exports = Logs;
